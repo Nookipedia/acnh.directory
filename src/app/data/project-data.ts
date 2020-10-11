fd30c2834e250777736d5f376733d2195bcf253c
KevinCCucumber <patrick.wegner@stud.hn.com>
2020-10-11 17:27:05 +0200

    Added Horizonpedia to site

--- src/app/data/project-data.ts
+++ src/app/data/project-data.ts
@@ -565,6 +565,32 @@ export const projectsData = [
         ]
     },
     {
+        name: "Horizonpedia",
+        description: "App for iOS and Android with Item catalog, fake art comparison, and more!",
+        languages: ["en", "es", "de", "fr", "it", "jp", "ko", "nl", "ru", "zh", "se"],
+        logo: "",
+        logoStyle: "",
+        site: "",
+        android: "https://play.google.com/store/apps/details?id=com.wegner.horizonpedia",
+        ios: "https://testflight.apple.com/join/cYhwMhdD",
+        discord: "https://discord.gg/pQMRkMg",
+        facebook: "",
+        instagram: "https://www.instagram.com/horizonpedia/",
+        twitter: "https://twitter.com/HorizonpediaE",
+        git: "",
+        information: [
+            "art",
+            "critters",
+            "fossils",
+            "items",
+            "villagers",
+        ],
+        tools: [
+            "checklist",
+            "fake art",
+        ]
+    },
+    {
         name: "icebr's AC:NH Helper",
         description: "Fish and bugs that are available for catching in AC:NH depend on location, time and current month",
         languages: ["en", "es", "de", "fr", "it", "jp", "ko", "nl", "ru", "zh"],
